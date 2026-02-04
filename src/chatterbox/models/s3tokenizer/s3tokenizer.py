7a94b544c05a81868ad617fc1401188c03460e8c
Wubpooz <mathieu.waharte@gmail.com>
2026-02-04 11:56:57 +0100

    fixing dep issue

--- src/chatterbox/models/s3tokenizer/s3tokenizer.py
+++ src/chatterbox/models/s3tokenizer/s3tokenizer.py
@@ -4,8 +4,8 @@ import numpy as np
 import librosa
 import torch
 import torch.nn.functional as F
-from s3tokenizer.utils import padding
-from s3tokenizer.model_v2 import (
+from .utils import padding
+from .model_v2 import (
     S3TokenizerV2,
     ModelConfig,
 )
